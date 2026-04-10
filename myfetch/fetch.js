37fc29d5e01fc1672e223198ff62b98ca038e957
Sahatsawat-Wattana <sahatsawat.wattanapraditchai@gmail.com>
2026-04-10 13:13:09 +0700

    add myfetch

--- myfetch/fetch.js
+++ myfetch/fetch.js
@@ -1,1 +1,20 @@
-const url = "";
+const context = "TIME_SERIES_DAILY";
+const apiKey = "";
+const symbol = "IBM";
+
+function callStock(context, symbol, apiKey) {
+  const url = `https://www.alphavantage.co/query?function=${context}&symbol=${symbol}&apikey=${apiKey}`;
+  const option = { headers: { "User-Agent": "request" } };
+
+  return fetch(url,option).then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  }).then((data) => {console.log(data)})
+  .catch((error) => {
+      console.error(error);
+    });
+}
+
+callStock(context,symbol,apiKey)
